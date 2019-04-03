90e88921e193fc26522a04d050ddcca17a4e23e1
Your Name <you@example.com>
2019-04-03 13:57:49 +0100

    functioning code, lots of fixes

--- src/cnn_classifier_train.py
+++ src/cnn_classifier_train.py
@@ -1,11 +1,13 @@
 import json
 import os
 import pickle
+import numpy as np
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
 from model.Models import CNNModel
-from model.eval.eval import f1_score
+from eval.eval import f1_score
+from utils.embedding_operations import read_embeddings
 
 # Global variables
 batch_size = 32
@@ -52,14 +54,14 @@ def prepare_minibatch(data, mesh_to_idx, word_to_idx):
 	for article in data:
 		word_seq = article['abstract'].lower().strip().split(' ')
 		idx_seq = [word_to_idx[word] if word in word_to_idx else word_to_idx['unk'] for word in word_seq]
-		idx_seq = idx[:max_seq_len]
+		idx_seq = idx_seq[:max_seq_len]
 		src_seq = np.zeros(max_seq_len)
 		src_seq[0:len(idx_seq)] = idx_seq
 		X.append(src_seq)
 		tgt_seq = np.zeros(len(mesh_to_idx))
 		tgt_idx = [mesh_to_idx[mesh] for mesh in article['mesh_labels']]
 		tgt_seq[tgt_idx] = 1
-		Y.append(tgt_idx)
+		Y.append(tgt_seq)
 		labels.append(article['mesh_labels'])
 	X = np.vstack(X)
 	Y = np.vstack(Y)
@@ -77,9 +79,9 @@ def train(model, criterion, mesh_to_idx, mesh_vocab, word_to_idx, src_vocab):
 		i = 0
 		while i < len(file_content):
 			input_idx_seq, target, _ = prepare_minibatch(file_content[i:i+batch_size], mesh_to_idx, word_to_idx)			
-			input_idx_seq = input_idx_seq.to(device)
-			target = target.to(device)
-			predict, _ = model(input_idx_seq, input_mask)
+			input_idx_seq = torch.tensor(input_idx_seq).to(device, dtype=torch.long)
+			target = torch.tensor(target).to(device, dtype=torch.float)
+			predict, _ = model(input_idx_seq)
 
 			# computing the loss over the prediction
 			loss = criterion(target, predict)
@@ -111,8 +113,8 @@ def validate(model, mesh_to_idx, mesh_vocab, word_to_idx, src_vocab):
 		i = 0
 		while i < len(file_content):
 			input_idx_seq, target, true_labels_batch = prepare_minibatch(file_content[i:i+batch_size], mesh_to_idx, word_to_idx)			
-			input_idx_seq = input_idx_seq.to(device)
-			predict, _ = model(input_idx_seq, input_mask)
+			input_idx_seq = input_idx_seq.to(device, dtype=torch.long)
+			predict, _ = model(input_idx_seq)
 			predict[predict>threshold] = 1
 			predict[predict<threshold] = 0
 			predict = predict.to('cpu').numpy()
@@ -131,6 +133,10 @@ def validate(model, mesh_to_idx, mesh_vocab, word_to_idx, src_vocab):
 	return f1_score_micro
 
 if __name__ == '__main__':
+	
+
+	# location of the toy dataset ---> this needs to be replaced with the final dataset
+	data_file = '../data/bioasq_dataset/toyMeSH_2017.json'
 
 	# create the vocabulary for the input 
 	src_vocab, word_to_idx = get_vocab(data_file)
@@ -144,27 +150,26 @@ if __name__ == '__main__':
 	for mesh, idx in mesh_to_idx.items():
 		mesh_vocab[idx] = mesh
 
-
-	# read source word embedding matrix
-	emb_mat_src = read_embeddings('../data/embeddings/word.processed.embeddings', src_vocab)
-	emb_mat_src = torch.tensor(emb_mat_src).to(out_device)
-
-
-	loss_criterion = torch.nn.BCEWithLogitsLoss(reduction="none")
-	optimizer = torch.optim.Adam(transformer.parameters(), lr=learning_rate, betas=(0.9,0.999))
-	# optimizer = torch.optim.SGD(transformer.parameters(), lr=learning_rate)
-
 	# setting different model parameters
 	n_src_vocab = len(src_vocab)
 	n_tgt_vocab = len(mesh_to_idx)
 	max_seq_len = 1000
 	d_word_vec = 200
 	dropout = 0.1
-	
+	learning_rate = 0.005
 
-	model = CNNModel(n_src_vocab, n_tgt_vocab, max_len_seq, d_word_vec, emb_mat_src, dropout=dropout)
+	# read source word embedding matrix
+	# emb_mat_src = read_embeddings('../data/embeddings/word.processed.embeddings', src_vocab)
+	emb_mat_src = np.random.standard_normal((n_src_vocab, 200))
+	emb_mat_src = torch.tensor(emb_mat_src).to(device)	
+
+	model = CNNModel(n_src_vocab, n_tgt_vocab, max_seq_len, d_word_vec, emb_mat_src, dropout=dropout)
 	model = nn.DataParallel(model, output_device=device)
 	model.to(device)
+
+	criterion = torch.nn.BCEWithLogitsLoss(reduction="none")
+	optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate, betas=(0.9,0.999))
+	# optimizer = torch.optim.SGD(transformer.parameters(), lr=learning_rate)
 
 	train(model, criterion, mesh_to_idx, mesh_vocab, word_to_idx, src_vocab)
 
--- src/model/Models.py
+++ src/model/Models.py
@@ -4,29 +4,25 @@ import torch.nn.functional as F
 import numpy as np
 
 class CNNModel(nn.Module):
-	 def __init__(
-			self,
-			n_src_vocab, n_tgt_vocab, len_max_seq, d_word_vec, emb_mat_src, dropout=0.1):
-
+	def __init__(self, n_src_vocab, n_tgt_vocab, len_max_seq, d_word_vec, emb_mat_src, dropout=0.1):
 		super().__init__()
-
 		n_src_vocab, d_word_vec = emb_mat_src.size()
 		self.src_word_emb = nn.Embedding(n_src_vocab, d_word_vec, padding_idx=0)
 		self.src_word_emb.load_state_dict({'weight': emb_mat_src})
-
 		self.conv_layer_1 = nn.Conv1d(d_word_vec, 256, 3, padding=1, stride=1)
 		self.conv_layer_2 = nn.Conv1d(256, 256, 3, padding=1, stride=1)
 		self.conv_layer_3 = nn.Conv1d(256, 256, 3, padding=1, stride=1)
 		self.conv_layer_4 = nn.Conv1d(256, 64, 3, padding=1, stride=1)
 		self.maxpool = nn.MaxPool1d(3)
-		self.fc_layer_1 = nn.Linear(len_max_seq//81, 256)
+		self.fc_layer_1 = nn.Linear((len_max_seq//81)*64, 256)
 		self.output_layer = nn.Linear(256, n_tgt_vocab)
 		self.relu_activation = nn.ReLU()
 
 
-	def forward(input_idxs, input_masks):
+	def forward(self, input_idxs):
 
 		output = self.src_word_emb(input_idxs)
+		output = output.permute(0,2,1)
 		output = self.conv_layer_1(output)
 		output = self.maxpool(output)
 		output = self.conv_layer_2(output)
